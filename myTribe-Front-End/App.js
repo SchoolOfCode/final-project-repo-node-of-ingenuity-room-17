a35d8847cf7d7bc3531e25fb9c36b15265202f04
Your Name <you@example.com>
2022-02-23 11:53:45 +0000

    date picker working

--- myTribe-Front-End/App.js
+++ myTribe-Front-End/App.js
@@ -1,15 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
-import { useState } from 'react';
-import AddChores from './screens/AddChores';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import { useState } from "react";
+import AddChores from "./screens/AddChores";
 
 export default function App() {
-	return <AddChores />;
+  return <AddChores />;
 }
 
 const styles = StyleSheet.create({
-	mainView: {
-		height: '100%',
-		width: '100%',
-	},
+  mainView: {
+    height: "100%",
+    width: "100%",
+  },
 });
--- myTribe-Front-End/screens/AddChores.js
+++ myTribe-Front-End/screens/AddChores.js
@@ -1,56 +1,92 @@
 import {
-	StyleSheet,
-	Text,
-	View,
-	Button,
-	KeyboardAvoidingView,
-	ScrollView,
-} from 'react-native';
-import { useState } from 'react';
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  KeyboardAvoidingView,
+  ScrollView,
+} from "react-native";
+import { useEffect, useState } from "react";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function AddChores() {
-	return (
-		<KeyboardAvoidingView style={styles.container}>
-			<Text style={styles.heading}>This week's chores</Text>
-			<Text style={styles.date}>21st February 2022</Text>
-			<ScrollView
-				showsVerticalScrollIndicator={false}
-				style={styles.choreList}
-			></ScrollView>
-			<View style={styles.btnContainer}>
-				<Button title='add chore' color='#FFBD00' />
-				<Button title='family management' color='#FFBD00' />
-				<Button title='logout' color='#FFBD00' />
-			</View>
-		</KeyboardAvoidingView>
-	);
+  const [date, setDate] = useState(new Date());
+  const [mode, setMode] = useState("date");
+  const [show, setShow] = useState(false);
+  const [text, setText] = useState("");
+
+  const onChange = (e, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+
+    let tempDate = new Date(currentDate);
+    let formDate = `${tempDate.getDate()} / ${
+      tempDate.getMonth() + 1
+    } / ${tempDate.getFullYear()}`;
+
+    setText(formDate);
+  };
+
+  const showMode = (currentMode) => {
+    setShow(true);
+    setMode(currentMode);
+  };
+
+  return (
+    <KeyboardAvoidingView style={styles.container}>
+      <Text style={styles.heading}>This week's chores</Text>
+      <Text style={styles.date}>21st February 2022</Text>
+      <ScrollView showsVerticalScrollIndicator={false} style={styles.choreList}>
+        <Button
+          title="due date"
+          color="#FFBD00"
+          onPress={() => showMode("date")}
+        ></Button>
+        {show && (
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            mode={mode}
+            is24Hour={true}
+            display="default"
+            onChange={onChange}
+          />
+        )}
+      </ScrollView>
+      <View style={styles.btnContainer}>
+        <Button title="add chore" color="#FFBD00" />
+        <Button title="family management" color="#FFBD00" />
+        <Button title="logout" color="#FFBD00" />
+      </View>
+    </KeyboardAvoidingView>
+  );
 }
 const styles = StyleSheet.create({
-	container: {
-		height: '100%',
-		width: '100%',
-		paddingTop: 100,
-		paddingBottom: 50,
-		paddingLeft: 50,
-		paddingRight: 50,
-		backgroundColor: '#F2F0F0',
-	},
+  container: {
+    height: "100%",
+    width: "100%",
+    paddingTop: 100,
+    paddingBottom: 50,
+    paddingLeft: 50,
+    paddingRight: 50,
+    backgroundColor: "#F2F0F0",
+  },
 
-	heading: {
-		fontSize: 30,
-		fontWeight: 'bold',
-	},
+  heading: {
+    fontSize: 30,
+    fontWeight: "bold",
+  },
 
-	date: {
-		color: '#AAAAAC',
-	},
+  date: {
+    color: "#AAAAAC",
+  },
 
-	choreList: {
-		marginTop: 50,
-	},
+  choreList: {
+    marginTop: 50,
+  },
 
-	btnContainer: {
-		paddingTop: 10,
-		paddingBottom: 10,
-	},
+  btnContainer: {
+    paddingTop: 10,
+    paddingBottom: 10,
+  },
 });
