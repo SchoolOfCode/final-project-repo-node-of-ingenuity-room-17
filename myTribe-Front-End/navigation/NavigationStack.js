458b26d8b343dbd919fc748484273431334469b9
Indya 2 <indyacmolloy@gmail.com>
2022-02-23 12:31:17 +0000

    added navigator

---
+++ myTribe-Front-End/navigation/NavigationStack.js
@@ -0,0 +1,19 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+import AddChores from "../screens/AddChores.js";
+import ChoreList from "../screens/ChoreList.js";
+import Family from "../screens/Family.js";
+import Login from "../screens/Login.js";
+import SignUp from "../screens/SignUp.js";
+import Welcome from "../screens/Welcome.js";
+
+const ChoresNavigator = createStackNavigator({
+  AddChores: AddChores,
+  ChoreList: ChoreList,
+  Family: Family,
+  Login: Login,
+  SignUp: SignUp,
+  Welcome: Welcome,
+});
+
+export default createAppContainer(ChoresNavigator);
