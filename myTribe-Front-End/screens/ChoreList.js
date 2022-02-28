e30dacf93fed9dfad5bdb0885c6499937f565698
Alexander Telecan <alexander@Alexanders-MacBook-Pro.local>
2022-02-28 15:12:42 +0000

    added functionality which allows users to add chores

--- myTribe-Front-End/screens/ChoreList.js
+++ myTribe-Front-End/screens/ChoreList.js
@@ -1,129 +1,111 @@
 import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  KeyboardAvoidingView,
-  ScrollView,
-} from "react-native";
-import { useState } from "react";
-import Chore from "../components/Chore";
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	KeyboardAvoidingView,
+	ScrollView,
+} from 'react-native';
+import { useState, useEffect } from 'react';
+import Chore from '../components/Chore';
+import getDateDisplay from '../utils/date';
 
 export default function ChoreList(props) {
-  const [chores, setChores] = useState([
-    {
-      id: 1,
-      title: "Tidy room",
-      member: "Jeff",
-      description: "Lorem ipsum lorem lorem ipsum lorem ipsum lorem",
-      dueDate: "21.02.2022",
-    },
-    {
-      id: 2,
-      title: "Tidy room",
-      member: "Jeff",
-      description: "Lorem ipsum lorem lorem ipsum lorem ipsum lorem",
-      dueDate: "21.02.2022",
-    },
-    {
-      id: 3,
-      title: "Tidy room",
-      member: "Jeff",
-      description: "Lorem ipsum lorem lorem ipsum lorem ipsum lorem",
-      dueDate: "21.02.2022",
-    },
-    {
-      id: 4,
-      title: "Tidy room",
-      member: "Jeff",
-      description: "Lorem ipsum lorem lorem ipsum lorem ipsum lorem",
-      dueDate: "21.02.2022",
-    },
-    {
-      id: 5,
-      title: "Tidy room",
-      member: "Jeff",
-      description: "Lorem ipsum lorem lorem ipsum lorem ipsum lorem",
-      dueDate: "21.02.2022",
-    },
-  ]);
+	const [chores, setChores] = useState([]);
+	const [family, setFamily] = useState(false);
+	const [currentMember, setCurrentMember] = useState(false);
+	const [date, setDate] = useState(false);
 
-  const getFamily = props.navigation.getParam("family");
-  const getFirstMember = props.navigation.getParam("member");
-  const getAllMembers = props.navigation.getParam("members");
-  const getDocRef = props.navigation.getParam("docRef");
+	const addFamilyChoresHandler = () => {
+		props.navigation.setParams({ routename: 'AddChores' });
+		props.navigation.navigate('AddChores', {
+			family: family,
+			member: currentMember,
+			date: date,
+		});
+	};
 
-  const addFamilychoresHandler = () => {
-    props.navigation.setParams({ routename: "AddChores" });
-    props.navigation.navigate("AddChores", {
-      family: getFamily,
-      member: getFirstMember,
-      members: getAllMembers,
-      docRef: getDocRef,
-    });
-  };
+	const familyManagementHandler = () => {
+		props.navigation.navigate({ routeName: 'Family' });
+	};
 
-  const familyManagementHandler = () => {
-    props.navigation.navigate({ routeName: "Family" });
-  };
-  return (
-    <KeyboardAvoidingView style={styles.container}>
-      <Text style={styles.heading}>This week's chores</Text>
-      <Text style={styles.date}>21st February 2022</Text>
-      <ScrollView showsVerticalScrollIndicator={false} style={styles.choreList}>
-        {chores.map((el) => (
-          <Chore
-            key={el.id}
-            title={el.title}
-            member={el.member}
-            description={el.description}
-            dueDate={el.dueDate}
-          />
-        ))}
-      </ScrollView>
-      <View style={styles.btnContainer}>
-        <Button
-          title="add chore"
-          color="#FFBD00"
-          onPress={addFamilychoresHandler}
-        />
-        <Button
-          onPress={familyManagementHandler}
-          title="family management"
-          color="#FFBD00"
-        />
-        <Button title="logout" color="#FFBD00" />
-      </View>
-    </KeyboardAvoidingView>
-  );
+	useEffect(() => {
+		const newFamily = props.navigation.getParam('family');
+		setFamily(newFamily);
+		setCurrentMember(newFamily.members[0].name);
+		if (newFamily.chores !== undefined) {
+			setChores(newFamily.chores);
+		}
+		setDate(getDateDisplay());
+	});
+
+	return (
+		<KeyboardAvoidingView style={styles.container}>
+			<Text style={styles.heading}>
+				{currentMember && `${currentMember}'s chores`}
+			</Text>
+			<Text style={styles.date}>{date && date}</Text>
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				style={styles.choreList}
+			>
+				{chores.length > 0 ? (
+					chores.map((el) => (
+						<Chore
+							key={el.id}
+							title={el.title}
+							member={el.member}
+							description={el.description}
+							dueDate={el.dueDate}
+						/>
+					))
+				) : (
+					<Text>You haven't added any chores.</Text>
+				)}
+			</ScrollView>
+			<View style={styles.btnContainer}>
+				<Button
+					title='add chore'
+					color='#FFBD00'
+					onPress={addFamilyChoresHandler}
+				/>
+				<Button
+					onPress={familyManagementHandler}
+					title='family management'
+					color='#FFBD00'
+				/>
+				<Button title='logout' color='#FFBD00' />
+			</View>
+		</KeyboardAvoidingView>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: "100%",
-    width: "100%",
-    paddingTop: 100,
-    paddingBottom: 50,
-    paddingLeft: 50,
-    paddingRight: 50,
-    backgroundColor: "#F2F0F0",
-  },
+	container: {
+		height: '100%',
+		width: '100%',
+		paddingTop: 100,
+		paddingBottom: 50,
+		paddingLeft: 50,
+		paddingRight: 50,
+		backgroundColor: '#F2F0F0',
+	},
 
-  heading: {
-    fontSize: 30,
-    fontWeight: "bold",
-  },
+	heading: {
+		fontSize: 30,
+		fontWeight: 'bold',
+	},
 
-  date: {
-    color: "#AAAAAC",
-  },
+	date: {
+		color: '#AAAAAC',
+	},
 
-  choreList: {
-    marginTop: 50,
-  },
+	choreList: {
+		marginTop: 50,
+	},
 
-  btnContainer: {
-    paddingTop: 10,
-    paddingBottom: 10,
-  },
+	btnContainer: {
+		paddingTop: 10,
+		paddingBottom: 10,
+	},
 });
